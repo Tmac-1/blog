97b1136256962650c04e54851364e028b9709fe0
renbinbin <1906526613@qq.com>
2023-07-05 09:51:38 +0800

    feat(组件):

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    // extends: ["@commitlint/config-conventional"],
-  extends: ["cz"],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(\w*)\(([\u4e00-\u9fa5]*)\)/,
-      headerCorrespondence: ["type", "scope"],
-    },
-  },
-  rules: {
-    "type-empty": [2, "never"],
-    "scope-empty": [2, "never"],
-  },
+    extends: ["@commitlint/config-conventional"],
+//   extends: ["cz"],
+//   parserPreset: {
+//     parserOpts: {
+//       headerPattern: /^(\w*)\(([\u4e00-\u9fa5]*)\)/,
+//       headerCorrespondence: ["type", "scope"],
+//     },
+//   },
+//   rules: {
+//     "type-empty": [2, "never"],
+//     "scope-empty": [2, "never"],
+//   },
+"rules": {"type-enum": [2,"always",["✨ 新功能","🐛 修复"]],"type-empty": [0],"subject-empty": [0]}
 };
